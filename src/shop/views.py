2eb52ff0a65b6616a08ac157ad658468cea6d208
Paulina Izienicka <paulaiz876@gmail.com>
2025-11-20 01:58:59 +0100

    Add About page and minor template fixes

--- src/shop/views.py
+++ src/shop/views.py
@@ -4,3 +4,6 @@ from .models import Donut
 def home(request):
     donuts = Donut.objects.filter(is_custom_base=False)
     return render(request, 'home.html', {'donuts':donuts})
+
+def about(request):
+    return render(request, 'about.html', {})
